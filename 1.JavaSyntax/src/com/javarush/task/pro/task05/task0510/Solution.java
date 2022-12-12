96a720b736387078a461b8a7dba96ee0e5988f3e
eigobud <igor.budnitski@ericsson.com>
2022-12-12 12:23:43 +0200

    Initial commit

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0510/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.pro.task05.task0510;
+
+/* 
+Треугольный массив
+*/
+
+public class Solution {
+
+    public static int[][] result = new int[10][];
+
+    public static void main(String[] args) {
+        int count = 0;
+        for (int i = 0; i < result.length; i++) {
+            result[i] = new int[i+1];
+        }
+        for (int i = 0; i < result.length; i++) {
+            for (int j = 0; j < result[i].length; j++) {
+                System.out.print(result[i][j]);
+            }
+            System.out.println();
+        }
+    }
+}
