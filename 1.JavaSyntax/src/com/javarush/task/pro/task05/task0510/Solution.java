71112491920e249bffde688469fd70a650924ec7
eigobud <igor.budnitski@ericsson.com>
2022-12-12 12:29:15 +0200

    Initial commit

--- 1.JavaSyntax/src/com/javarush/task/pro/task05/task0510/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0510/Solution.java
@@ -9,13 +9,20 @@ public class Solution {
     public static int[][] result = new int[10][];
 
     public static void main(String[] args) {
-        int count = 0;
+
         for (int i = 0; i < result.length; i++) {
             result[i] = new int[i+1];
         }
+
         for (int i = 0; i < result.length; i++) {
             for (int j = 0; j < result[i].length; j++) {
-                System.out.print(result[i][j]);
+                result[i][j] = i+j;
+            }
+        }
+
+        for (int i = 0; i < result.length; i++) {
+            for (int j = 0; j < result[i].length; j++) {
+                System.out.print(result[i][j] + " ");
             }
             System.out.println();
         }
