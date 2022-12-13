0fd1fcf83080660380f807967408883cbded6a44
eigobud <igor.budnitski@ericsson.com>
2022-12-13 21:38:52 +0200

    Initial commit

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task06/task0602/Solution.java
@@ -0,0 +1,12 @@
+package com.javarush.task.pro.task06.task0602;
+
+/* 
+Упорядоченная информация
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("Я — поэт, зовусь я Цветик.");
+        System.out.print("От меня вам всем приветик.");
+    }
+}
