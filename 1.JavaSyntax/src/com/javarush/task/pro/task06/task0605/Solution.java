285b283f9dc501b807307d7cf9f5b759533496a7
igor-budnitski <96424046+igor-budnitski@users.noreply.github.com>
2022-12-14 10:11:10 +0200

    Initial commit

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task06/task0605/Solution.java
@@ -0,0 +1,33 @@
+package com.javarush.task.pro.task06.task0605;
+
+/* 
+Правильный порядок
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int[] array = {10, 11, 22, 33, 44, 55, 66, 77, 88, 99};
+        printArray(array);
+        reverseArray(array);
+        printArray(array);
+    }
+
+    public static void reverseArray(int[] array) {
+        int temp;
+        int count = array.length-1;
+        for (int i = 0; i < array.length/2; i++) {
+            temp = array[i];
+            array[i] = array[count];
+            array[count] = temp;
+            count--;
+        }
+    }
+
+    public static void printArray(int[] array) {
+        for (int i : array) {
+            System.out.print(i + ", ");
+        }
+        System.out.println();
+    }
+}
