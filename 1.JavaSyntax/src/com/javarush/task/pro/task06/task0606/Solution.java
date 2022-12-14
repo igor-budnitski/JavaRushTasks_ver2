120f4680c0dc1ce0a22b3bd4dec8ae0f15c610d5
igor-budnitski <96424046+igor-budnitski@users.noreply.github.com>
2022-12-14 16:12:19 +0200

    Initial commit

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task06/task0606/Solution.java
@@ -0,0 +1,54 @@
+package com.javarush.task.pro.task06.task0606;
+
+/* 
+Универсальный солдат
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void universalMethod(int number) {
+
+    }
+
+    public static void universalMethod(double number) {
+
+    }
+
+    public static void universalMethod(String name) {
+
+    }
+
+    public static void universalMethod(String name, int age) {
+
+    }
+
+    public static void universalMethod(String name, int age, String rank) {
+
+    }
+
+    public static void universalMethod(float number) {
+
+    }
+
+    public static void universalMethod(byte number) {
+
+    }
+
+    public static void universalMethod(String test, float number) {
+
+    }
+
+    public static void universalMethod(String name, String LastName, boolean isDead) {
+
+    }
+
+    public static void universalMethod(String name, String LastName, boolean isDead, int age) {
+
+    }
+
+    //напишите тут ваш код
+}
