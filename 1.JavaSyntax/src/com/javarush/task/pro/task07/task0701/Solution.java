b5c8cb268359a1f196cd27f12a6ef64132d4ee12
eigobud <igor.budnitski@ericsson.com>
2022-12-16 09:46:47 +0200

    Initial commit

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0701/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.pro.task07.task0701;
+
+/* 
+Все возможные типы
+*/
+
+public class Solution {
+    //напишите тут ваш код
+    byte aByte = 3;
+    short aShort = 2;
+    int anInt = 23;
+    long aLong = 234L;
+    double aDouble = 2.3;
+    float aFloat = 2.345f;
+    boolean aBoolean = true;
+    char aChar = 'a';
+}
