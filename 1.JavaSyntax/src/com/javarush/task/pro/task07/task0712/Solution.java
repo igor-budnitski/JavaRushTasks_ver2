8006a9dcf99eae145efa822c3e0ba057007e64e8
eigobud <igor.budnitski@ericsson.com>
2022-12-19 18:23:32 +0200

    Initial commit

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0712/Solution.java
@@ -0,0 +1,31 @@
+package com.javarush.task.pro.task07.task0712;
+
+/* 
+Первые объекты
+*/
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        String s1 = new String("New String Object");
+        String s2 = new String("New String Object");
+        String s3 = new String("New String Object");
+        String s4 = new String("New String Object");
+        String s5 = new String("New String Object");
+        String s6 = new String("New String Object");
+        String s7 = new String("New String Object");
+        String s8 = new String("New String Object");
+        String s9 = new String("New String Object");
+        String s10 = new String("New String Object");
+
+        int[] array1 = new int[10];
+        int[] array2 = new int[10];
+        int[] array3 = new int[10];
+        int[] array4 = new int[10];
+        int[] array5 = new int[10];
+
+        Scanner sc1 = new Scanner(System.in);
+        Scanner sc2 = new Scanner(System.in);
+    }
+}
