0dfccca2596e1f3cee2429bec50a54fa81fd9281
eigobud <igor.budnitski@ericsson.com>
2023-01-14 13:59:09 +0200

    Commit

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task10/task1006/Solution.java
@@ -0,0 +1,13 @@
+package com.javarush.task.pro.task10.task1006;
+
+/* 
+Кто тут наследник?
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        HybridCar hb = new HybridCar();
+        GasCar gc = new GasCar();
+        ElectricCar ec = new ElectricCar();
+    }
+}
