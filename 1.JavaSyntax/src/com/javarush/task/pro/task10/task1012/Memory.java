c55a3da3e148cc8ca92e0c498a75038845659389
eigobud <igor.budnitski@ericsson.com>
2023-01-19 20:15:23 +0200

    Commit

--- 1.JavaSyntax/src/com/javarush/task/pro/task10/task1012/Memory.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task10/task1012/Memory.java
@@ -10,15 +10,23 @@ import java.util.Objects;
 public class Memory {
 
     public static void main(String[] args) {
-        String[] memory = {"object15", null, null, "object2", null, null, null, "object32", null, "object4"};
+        String[] memory = {"object15", null, null, "object2", null,
+                null, null, "object32", null, "object4"};
         executeDefragmentation(memory);
         System.out.println(Arrays.toString(memory));
     }
 
     public static void executeDefragmentation(String[] array) {
-        String temp;
         for (int i = 0; i < array.length; i++) {
-
+            if (array[i] == null){
+                for (int j = i + 1; j < array.length; j++) {
+                    if (array[j] != null){
+                        array[i] = array[j];
+                        array[j] = null;
+                        break;
+                    }
+                }
+            }
         }
     }
 }
