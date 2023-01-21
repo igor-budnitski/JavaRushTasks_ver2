b51916b88711e16781070cddf18156578924ab4f
igor-budnitski <96424046+igor-budnitski@users.noreply.github.com>
2023-01-21 19:36:31 +0200

    Commit

--- 1.JavaSyntax/src/com/javarush/task/pro/task10/task1021/Car.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task10/task1021/Car.java
@@ -9,6 +9,10 @@ public class Car {
     private int year;
     private String color;
 
-    //напишите тут ваш код
+    public void initialize(String model, int year, String color){
+        this.model = model;
+        this.year = year;
+        this.color = color;
+    }
 
 }
