797a20868fcdf4194798b86628d1d2e0edaaaf63
eigobud <igor.budnitski@ericsson.com>
2023-01-21 17:55:07 +0200

    Commit

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task10/task1020/Computer.java
@@ -0,0 +1,8 @@
+package com.javarush.task.pro.task10.task1020;
+
+public class Computer {
+    Keyboard kb = new Keyboard();
+    Monitor mt = new Monitor();
+    Mouse ms = new Mouse();
+    SystemUnit su = new SystemUnit();
+}
