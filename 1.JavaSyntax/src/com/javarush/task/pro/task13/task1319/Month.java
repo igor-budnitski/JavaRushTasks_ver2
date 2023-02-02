fa03355a221eb94ee2d739ea8bd33bc649f51fff
eigobud <igor.budnitski@ericsson.com>
2023-02-02 20:05:27 +0200

    Commit

--- 1.JavaSyntax/src/com/javarush/task/pro/task13/task1319/Month.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task13/task1319/Month.java
@@ -16,10 +16,38 @@ public enum Month {
     SEPTEMBER,
     OCTOBER,
     NOVEMBER,
-    DECEMBER
+    DECEMBER;
 
-    public static String[] getWinterMonths(){
-        String[] array = new String[3];
+
+    public static Month[] getWinterMonths(){
+        Month[] array = new Month[3];
+        array[0] = Month.DECEMBER;
+        array[1] = Month.JANUARY;
+        array[2] = Month.FEBRUARY;
+        return array;
+    }
+
+    public static Month[] getSpringMonths(){
+        Month[] array = new Month[3];
+        array[0] = Month.MARCH;
+        array[1] = Month.APRIL;
+        array[2] = Month.MAY;
+        return array;
+    }
+
+    public static Month[] getSummerMonths(){
+        Month[] array = new Month[3];
+        array[0] = Month.JUNE;
+        array[1] = Month.JULY;
+        array[2] = Month.AUGUST;
+        return array;
+    }
+
+    public static Month[] getAutumnMonths(){
+        Month[] array = new Month[3];
+        array[0] = Month.SEPTEMBER;
+        array[1] = Month.OCTOBER;
+        array[2] = Month.NOVEMBER;
         return array;
     }
 
